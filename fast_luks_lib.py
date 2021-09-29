95975b7a273b67daf131c29a969c0988b5fc6751
dacolombo <daniele.colombo39@gmail.com>
2021-09-29 22:12:09 +0200

    Make echo and logs functions reusable

--- fast_luks_lib.py
+++ fast_luks_lib.py
@@ -7,16 +7,19 @@ from string import ascii_letters, digits, ascii_lowercase
 import subprocess
 import os
 from pathlib import Path
+import shutil
 from datetime import datetime
 import re
 import zc.lockfile
 from configparser import ConfigParser
+import venv
 
 
 ################################################################################
 # VARIABLES
 alphanum = ascii_letters + digits
 LOGFILE = '/tmp/fast_luks.log'
+time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 now = datetime.now().strftime('-%b-%d-%y-%H%M%S')
 
 
@@ -50,46 +53,27 @@ def intro():
 # INFO
 # WARNING
 # ERROR
-time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
 # Echo functions
-def echo_info(info):
-    info = f'INFO {time} {info}\n'
-    print(info)
-    return info
 
-def echo_debug(debug):
-    debug = f'DEBUG {time} {debug}\n'
-    print(debug)
-    return debug
+def check_loglevel(loglevel):
+    valid_loglevels = ['INFO','DEBUG','WARNING','ERROR']
+    if loglevel not in valid_loglevels:
+        raise ValueError(f'loglevel must be one of {valid_loglevels}')
 
-def echo_warn(warn):
-    warn = f'WARNING {time} {warn}\n'
-    print(warn)
-    return warn
 
-def echo_error(error):
-    error = f'ERROR {time} {error}\n'
-    print(error)
-    return error
+def echo(loglevel, text):
+    check_loglevel(loglevel)
+    message = f'{loglevel} {time} {text}\n'
+    print(message)
+    return message
 
 
 # Logs functions
-def logs_info(info):
-    with open(LOGFILE, 'a') as log:
-        log.write(echo_info(info))
-
-def logs_debug(debug):
-    with open(LOGFILE, 'a') as log:
-        log.write(echo_debug(debug))
-
-def logs_warn(warn):
-    with open(LOGFILE, 'a') as log:
-        log.write(echo_warn(warn))
-
-def logs_error(error):
-    with open(LOGFILE, 'a') as log:
-        log.write(echo_error(error))
+def logs(loglevel, text):
+    check_loglevel(loglevel)
+    with open(LOGFILE, 'a+') as log:
+        log.write(echo(loglevel, text))
 
 
 #________________________________
@@ -138,7 +122,7 @@ def lock(LOCKFILE):
         with open(LOCKFILE, 'r') as lock_file:
             pid_hostname = lock_file.readline()
             PID = re.search(r'^\s(\d+);', pid_hostname).group()
-        echo_error(f'Another script instance is active: PID {PID}')
+        echo('ERROR', f'Another script instance is active: PID {PID}')
         exit(2)
 
     # if lock is stale, remove it and restart
@@ -148,16 +132,16 @@ def lock(LOCKFILE):
     #    os.kill(PID, 0)
     #except OSError:
     #    # lock is stale, remove it and restart
-    #    echo_debug(f'Removing fake lock file of nonexistant PID {PID}')
+    #    echo('DEBUG',f'Removing fake lock file of nonexistant PID {PID}')
     #    os.remove(LOCKFILE)
-    #    echo_debug('Restarting luks script')
+    #    echo('DEBUG','Restarting luks script')
     #   TODO: re-run script
 
     # lock is valid and OTHERPID is active - exit, we're locked!
-    echo_error(f'Lock failed, PID {PID} is active')
-    echo_error(f'Another {STAT} process is active')
-    echo_error(f'If you are sure {STAT} is not already running,')
-    echo_error(f'You can remove {LOCKFILE} and restart {STAT}')
+    echo('ERROR', f'Lock failed, PID {PID} is active')
+    echo('ERROR', f'Another {STAT} process is active')
+    echo('ERROR', f'If you are sure {STAT} is not already running,')
+    echo('ERROR', f'You can remove {LOCKFILE} and restart {STAT}')
     exit(2)
 
 
@@ -174,66 +158,66 @@ def create_random_cryptdev_name():
 
 #____________________________________
 def info(device, cipher_algorithm, hash_algorithm, keysize, cryptdev, mountpoint, filesystem):
-    echo_debug(f'LUKS header information for {device}')
-    echo_debug(f'Cipher algorithm: {cipher_algorithm}')
-    echo_debug(f'Hash algorithm {hash_algorithm}')
-    echo_debug(f'Keysize: {keysize}')
-    echo_debug(f'Device: {device}')
-    echo_debug(f'Crypt device: {cryptdev}')
-    echo_debug(f'Mapper: /dev/mapper/{cryptdev}')
-    echo_debug(f'Mountpoint: {mountpoint}')
-    echo_debug(f'File system: {filesystem}')
+    echo('DEBUG', f'LUKS header information for {device}')
+    echo('DEBUG', f'Cipher algorithm: {cipher_algorithm}')
+    echo('DEBUG', f'Hash algorithm {hash_algorithm}')
+    echo('DEBUG', f'Keysize: {keysize}')
+    echo('DEBUG', f'Device: {device}')
+    echo('DEBUG', f'Crypt device: {cryptdev}')
+    echo('DEBUG', f'Mapper: /dev/mapper/{cryptdev}')
+    echo('DEBUG', f'Mountpoint: {mountpoint}')
+    echo('DEBUG', f'File system: {filesystem}')
 
 
 #____________________________________
 # Install cryptsetup
 def install_cryptsetup(log_stderr_stdout=False):
     if DISTNAME == 'ubuntu':
-        echo_info('Distribution: Ubuntu. Using apt.')
+        echo('INFO', 'Distribution: Ubuntu. Using apt.')
         run_command('apt-get install -y cryptsetup pv', log_stderr_stdout)
     else:
-        echo_info('Distribution: CentOS. Using yum.')
+        echo('INFO', 'Distribution: CentOS. Using yum.')
         run_command('yum install -y cryptsetup-luks pv', log_stderr_stdout)
 
 
 #____________________________________
 # Check cryptsetup installation
 def check_cryptsetup():
-    echo_info('Check if the required applications are installed...')
+    echo('INFO', 'Check if the required applications are installed...')
     
     _, _, dmsetup_status = run_command('type -P dmsetup &>/dev/null')
     if dmsetup_status != 0:
-        echo_info('dmsetup is not installed. Installing...') # TODO: add install device_mapper
+        echo('INFO', 'dmsetup is not installed. Installing...') # TODO: add install device_mapper
     
     _, _, cryptsetup_status = run_command('type -P cryptsetup &>/dev/null')
     if cryptsetup_status != 0:
-        echo_info('cryptsetup is not installed. Installing...')
+        echo('INFO', 'cryptsetup is not installed. Installing...')
         install_cryptsetup(log_stderr_stdout=True)
-        echo_info('cryptsetup installed.')
+        echo('INFO', 'cryptsetup installed.')
 
 
 #____________________________________
 # Check volume 
 def check_vol(mountpoint, device):
-    logs_debug('Checking storage volume.')
+    logs('DEBUG', 'Checking storage volume.')
 
     #num_mountpoint, _, _ = run_command(f'mount | grep -c {mountpoint}')
     if os.path.ismount(mountpoint):
         device, _, _ = run_command(f'df -P {mountpoint} | tail -1 | cut -d" " -f 1')
-        logs_debug(f'Device name: {device}')
+        logs('DEBUG', f'Device name: {device}')
 
     else:
         if Path.is_block_device(Path(device)):
-            logs_debug(f'External volume on {device}. Using it for encryption.')
+            logs('DEBUG', f'External volume on {device}. Using it for encryption.')
             if not os.path.isdir(mountpoint):
-                logs_debug(f'Creating {mountpoint}')
+                logs('DEBUG', f'Creating {mountpoint}')
                 os.makedirs(mountpoint, exist_ok=True)
-                logs_debug(f'Device name: {device}')
-                logs_debug(f'Mountpoint: {mountpoint}')
+                logs('DEBUG', f'Device name: {device}')
+                logs('DEBUG', f'Mountpoint: {mountpoint}')
         else:
-            logs_error('Device not mounted, exiting!')
-            logs_error('Please check logfile:')
-            logs_error(f'No device mounted to {mountpoint}')
+            logs('ERROR', 'Device not mounted, exiting!')
+            logs('ERROR', 'Please check logfile:')
+            logs('ERROR', f'No device mounted to {mountpoint}')
             run_command('df -h', log_stderr_stdout=True)
             unlock(lock, LOCKFILE)
             exit(1)
@@ -242,10 +226,10 @@ def check_vol(mountpoint, device):
 # Check if the volume is already encrypted.
 # If yes, skip the encryption
 def lsblk_check(device):
-    logs_debug('Checking if the volume is already encrypted.')
+    logs('DEBUG', 'Checking if the volume is already encrypted.')
     devices, _, _ = run_command('lsblk -p -o NAME,FSTYPE')
     if re.search(f'{device}\s+crypto_LUKS', devices):
-            logs_info('The volume is already encrypted')
+            logs('INFO', 'The volume is already encrypted')
             return True
     else:
         return False
@@ -254,9 +238,9 @@ def lsblk_check(device):
 #____________________________________
 # Umount volume
 def umount_vol(mountpoint, device):
-    logs_info('Umounting device.')
+    logs('INFO', 'Umounting device.')
     run_command(f'umount {mountpoint}', log_stderr_stdout=True)
-    logs_info(f'{device} umounted, ready for encryption!')
+    logs('INFO', f'{device} umounted, ready for encryption!')
 
 
 #____________________________________
@@ -269,26 +253,26 @@ def create_random_secret(passphrase_length):
 def setup_device(device, cryptdev, mountpoint, filesystem, #vault_url, wrapping_token, secret_path, user_key,
                  luks_header_backup_dir, luks_header_backup_file, lock, LOCKFILE, cipher_algorithm='aes-xts-plain64', keysize=256,
                  hash_algorithm='sha256', passphrase_length=None, passphrase=None, passphrase_confirmation=None):
-    echo_info('Start the encryption procedure.')
-    logs_info(f'Using {cipher_algorithm} algorithm to luksformat the volume.')
-    logs_debug('Start cryptsetup')
+    echo('INFO', 'Start the encryption procedure.')
+    logs('INFO', f'Using {cipher_algorithm} algorithm to luksformat the volume.')
+    logs('DEBUG', 'Start cryptsetup')
     info(device, cipher_algorithm, hash_algorithm, keysize, cryptdev, mountpoint, filesystem)
-    logs_debug('Cryptsetup full command:')
-    logs_debug('cryptsetup -v --cipher $cipher_algorithm --key-size $keysize --hash $hash_algorithm --iter-time 2000 --use-urandom --verify-passphrase luksFormat $device --batch-mode')
+    logs('DEBUG', 'Cryptsetup full command:')
+    logs('DEBUG', 'cryptsetup -v --cipher $cipher_algorithm --key-size $keysize --hash $hash_algorithm --iter-time 2000 --use-urandom --verify-passphrase luksFormat $device --batch-mode')
 
     if passphrase_length == None:
         if passphrase == None:
-            echo_error("Missing passphrase!")
+            echo('ERROR', "Missing passphrase!")
             unlock(lock, LOCKFILE)
             exit(1)
         if passphrase_confirmation == None:
-            echo_error('Missing confirmation passphrase!')
+            echo('ERROR', 'Missing confirmation passphrase!')
             unlock(lock, LOCKFILE)
             exit(1)
         if passphrase == passphrase_confirmation:
             s3cret = passphrase
         else:
-            echo_error('No matching passphrases!')
+            echo('ERROR', 'No matching passphrases!')
             unlock(lock, LOCKFILE)
             exit(1)
     else:
@@ -316,27 +300,27 @@ def setup_device(device, cryptdev, mountpoint, filesystem, #vault_url, wrapping_
         # 3 out of memory
         # 4 wrong device specified
         # 5 device already exists or device is busy.
-        logs_error(f'Command cryptsetup failed with exit code {luksHeaderBackup_ec}! Mounting {device} to {mountpoint} and exiting.')
+        logs('ERROR', f'Command cryptsetup failed with exit code {luksHeaderBackup_ec}! Mounting {device} to {mountpoint} and exiting.')
         if luksHeaderBackup_ec == 2:
-             echo_error('Bad passphrase. Please try again.')
+             echo('ERROR', 'Bad passphrase. Please try again.')
         unlock(lock, LOCKFILE)
         exit(luksHeaderBackup_ec)
 
 #____________________________________
 def open_device(cryptdev, s3cret, device, mountpoint):
-    echo_info('Open LUKS volume')
+    echo('INFO', 'Open LUKS volume')
     if not Path(f'/dev/mapper/{cryptdev}').is_block_device():
         _, _, openec = run_command(f'printf "{s3cret}\n" | cryptsetup luksOpen {device} {cryptdev}')
         if openec != 0:
             if openec == 2:
-                echo_error('Bad passphrase. Please try again.')
+                echo('ERROR', 'Bad passphrase. Please try again.')
                 unlock(lock, LOCKFILE)
                 exit(openec)
             else:
-                echo_error(f'Crypt device already exists! Please check logs: {LOGFILE}')
-                logs_error('Unable to luksOpen device.')
-                logs_error(f'/dev/mapper/{cryptdev} already exists.')
-                logs_error(f'Mounting {device} to {mountpoint} again.')
+                echo('ERROR', f'Crypt device already exists! Please check logs: {LOGFILE}')
+                logs('ERROR', 'Unable to luksOpen device.')
+                logs('ERROR', f'/dev/mapper/{cryptdev} already exists.')
+                logs('ERROR', f'Mounting {device} to {mountpoint} again.')
                 run_command(f'mount {device} {mountpoint}', log_stderr_stdout=True)
                 unlock(lock, LOCKFILE)
                 exit(1)
@@ -344,7 +328,7 @@ def open_device(cryptdev, s3cret, device, mountpoint):
 
 #____________________________________
 def encryption_status(cryptdev):
-    logs_info(f'Check {cryptdev} status with cryptsetup status')
+    logs('INFO', f'Check {cryptdev} status with cryptsetup status')
     run_command(f'cryptsetup -v status {cryptdev}', log_stderr_stdout=True)
 
 
@@ -357,32 +341,32 @@ def encryption_status(cryptdev):
 # Before encrypting a drive, it is recommended to perform a secure erase of the disk by overwriting the entire drive with random data.
 # To prevent cryptographic attacks or unwanted file recovery, this data is ideally indistinguishable from data later written by dm-crypt.
 def wipe_data():
-    echo_info('Paranoid mode selected. Wiping disk')
-    logs_info('Wiping disk data by overwriting the entire drive with random data.')
-    logs_info('This might take time depending on the size & your machine!')
+    echo('INFO', 'Paranoid mode selected. Wiping disk')
+    logs('INFO', 'Wiping disk data by overwriting the entire drive with random data.')
+    logs('INFO', 'This might take time depending on the size & your machine!')
     
     run_command(f'dd if=/dev/zero of=/dev/mapper/{cryptdev} bs=1M status=progress')
     
-    logs_info(f'Block file /dev/mapper/{cryptdev} created.')
-    logs_info('Wiping done.')
+    logs('INFO', f'Block file /dev/mapper/{cryptdev} created.')
+    logs('INFO', 'Wiping done.')
 
 
 #____________________________________
 def create_fs(filesystem, cryptdev):
-    echo_info('Creating filesystem.')
-    logs_info(f'Creating {filesystem} filesystem on /dev/mapper/{cryptdev}')
+    echo('INFO', 'Creating filesystem.')
+    logs('INFO', f'Creating {filesystem} filesystem on /dev/mapper/{cryptdev}')
     _, _, mkfs_ec = run_command(f'mkfs -t {filesystem} /dev/mapper/{cryptdev}', log_stderr_stdout=True)
     if mkfs_ec != 0:
-        echo_error(f'While creating {filesystem} filesystem. Please check logs: {LOGFILE}')
-        echo_error('Command mkfs failed!')
+        echo('ERROR', f'While creating {filesystem} filesystem. Please check logs: {LOGFILE}')
+        echo('ERROR', 'Command mkfs failed!')
         unlock(lock, LOCKFILE)
         exit(1)
 
 
 #____________________________________
 def mount_vol(cryptdev, mountpoint):
-    echo_info('Mounting encrypted device.')
-    logs_info(f'Mounting /dev/mapper/{cryptdev} to {mountpoint}')
+    echo('INFO', 'Mounting encrypted device.')
+    logs('INFO', f'Mounting /dev/mapper/{cryptdev} to {mountpoint}')
     run_command(f'mount /dev/mapper/{cryptdev} {mountpoint}', log_stderr_stdout=True)
     run_command('df -Hv', log_stderr_stdout=True)
 
@@ -391,7 +375,7 @@ def mount_vol(cryptdev, mountpoint):
 def create_cryptdev_ini_file(luks_cryptdev_file, device, cipher_algorithm, hash_algorithm, keysize, cryptdev, mountpoint,
                              filesystem, luks_header_backup_dir, luks_header_backup_file):
     if not os.path.isfile(luks_cryptdev_file):
-        logs_debug(f'Create {luks_cryptdev_file}')
+        logs('DEBUG', f'Create {luks_cryptdev_file}')
         run_command(f'install -D /dev/null {luks_cryptdev_file}')
     
     luksUUID, _, _ = run_command(f'cryptsetup luksUUID {device}')
@@ -427,7 +411,7 @@ def end_encrypt_procedure(SUCCESS_FILE):
     # send signal to unclok waiting condition for automation software (e.g Ansible)
     with open(SUCCESS_FILE, 'w') as success_file:
         success_file.write('LUKS encryption completed.') # WARNING DO NOT MODFIFY THIS LINE, THIS IS A CONTROL STRING FOR ANSIBLE
-    echo_info('SUCCESSFUL.')
+    echo('INFO', 'SUCCESSFUL.')
 
 
 #____________________________________
@@ -435,16 +419,16 @@ def end_volume_setup_procedure(SUCCESS_FILE):
     # send signal to unclok waiting condition for automation software (e.g Ansible)
     with open(SUCCESS_FILE,'w') as success_file:
         success_file.write('Volume setup completed.')
-    echo_info('SUCCESSFUL.')
+    echo('INFO', 'SUCCESSFUL.')
 
 
 #____________________________________
 def load_default_config():
     if os.path.isfile('./defaults.conf'):
-        logs_info('Loading default configuration from defaults.conf')
+        logs('INFO', 'Loading default configuration from defaults.conf')
         import defaults.conf
     else:
-        logs_info('No defaults.conf file found. Loading built-in variables.')
+        logs('INFO', 'No defaults.conf file found. Loading built-in variables.')
         global cipher_algorithm, keysize, hash_algorithm, device, cryptdev, mountpoint, filesystem, paranoid, non_interactive, foreground, luks_cryptdev_file, luks_header_backup
         cipher_algorithm='aes-xts-plain64'
         keysize=256
